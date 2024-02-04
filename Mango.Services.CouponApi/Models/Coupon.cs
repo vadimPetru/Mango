5aa24df8e505176614ea448db8aaecfed134e667
Vadim Petrulenko <66614@gmail.com>
2024-02-04 18:47:31 +0300

    Add CreateModel and CouponDto

---
+++ Mango.Services.CouponApi/Models/Coupon.cs
@@ -0,0 +1,23 @@
+﻿namespace Mango.Services.CouponApi.Models
+{
+    public class Coupon
+    {
+        /// <summary>
+        /// Индификатор
+        /// </summary>
+        public int CouponId { get; set; }
+        /// <summary>
+        /// Код купона
+        /// </summary>
+        public string CouponCode { get; set; }
+        /// <summary>
+        /// Скидка
+        /// </summary>
+        public double DiscountAmount { get; set; }
+        /// <summary>
+        /// Минимальное количество
+        /// </summary>
+        public int MinAmount { get; set; }
+
+    }
+}
