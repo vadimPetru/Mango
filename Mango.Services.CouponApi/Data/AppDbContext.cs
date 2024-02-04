c65311eebf041daf8c5796435cd4db3cef37171e
Vadim Petrulenko <66614@gmail.com>
2024-02-04 19:00:48 +0300

    Create Db context and prepare Couponmodels

---
+++ Mango.Services.CouponApi/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Services.CouponApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option)
+        {
+            
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
--- Mango.Services.CouponApi/Models/Coupon.cs
+++ Mango.Services.CouponApi/Models/Coupon.cs
@@ -1,18 +1,23 @@
-﻿namespace Mango.Services.CouponApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.CouponApi.Models
 {
     public class Coupon
     {
         /// <summary>
         /// Индификатор
         /// </summary>
+        [Key]
         public int CouponId { get; set; }
         /// <summary>
         /// Код купона
         /// </summary>
+        [Required]
         public string CouponCode { get; set; }
         /// <summary>
         /// Скидка
         /// </summary>
+        [Required]
         public double DiscountAmount { get; set; }
         /// <summary>
         /// Минимальное количество
